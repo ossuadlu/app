c2f8a3b2f7854f5b37e94271866a556e5c258dab
adny osorio <Huertaslina61@gmail.com>
2022-03-30 20:28:13 -0500

    se realizo cambios en generador token

--- JS/generadorToken.js
+++ JS/generadorToken.js
@@ -1,1 +1,30 @@
-export let TOKEN ="Bearer BQDeW2CINuQ1H45ChfGgcNc5Ce_khl29JyX0r5AhxdBjTNgSIgHDbr6-j-GQ-1P4_axyT_epjprQhmZtVbJzR4eHXkr6lDglHuCJbWLtTm5eDnnW46NPUH3cU9nKM_66vyu_ADNtl1BFg115AFCQeSVtpTlmecs"
+async function generarToken(){
+
+    let URI="https://accounts.spotify.com/api/token"
+
+    let client_id="client_id=ca7290cde70e4e208cff7259f1312795"
+    let client_secret="client_secret=6b22e65566a147119fd455df5caac481"
+    let grant_type="grant_type=client_credentials"
+
+    let parametros={
+        method:"POST",
+        headers:{"Content-Type":"application/x-www-form-urlencoded"},
+        body:`${client_id}&${client_secret}&${grant_type}`
+    }
+
+    "Bearer BQAaK7oP2vkklHszntkMJXP4CPBMM-i9QqxQ8kBpTRm-fVyHRyNXL_PGjx4dNRGzsrVGlqnvEIEDCvWLZd8"
+
+    fetch(URI,parametros)
+    .then(function(respuesta){
+        return(respuesta.json())
+    })
+    .then(function(respuesta){
+        console.log(respuesta)
+    })
+    .catch(function(respuesta){
+        console.log(respuesta)
+    })
+
+}
+
+generarToken()
